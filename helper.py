b412a6e46daa5251784125604f863d5d51360eff
bzgec <luclul103@gmail.com>
2021-11-20 23:17:10 +0100

    Fix W504 line break after binary operator

--- helper.py
+++ helper.py
@@ -141,6 +141,6 @@ def promptYesNo(question):
 def isRunningInDocker():
     path = '/proc/self/cgroup'
     return (
-        os.path.exists('/.dockerenv') or
-        os.path.isfile(path) and any('docker' in line for line in open(path))
+        os.path.exists('/.dockerenv')
+        or os.path.isfile(path) and any('docker' in line for line in open(path))
     )
